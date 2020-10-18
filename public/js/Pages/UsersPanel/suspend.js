97542557e61ef029298be7620f1de979b4528b73
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2020-10-18 14:15:32 -0300

    Updating jQuery and Bootstrap versions

---
+++ public/js/Pages/UsersPanel/suspend.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $('.suspend').click(function(e) {
+    e.preventDefault();
+
+    const tr = $(this).closest('tr');
+    const action = `${DIRPAGE}userspanel/suspend/${$(tr).attr('data-item')}`;
+    const error = $(tr).find('.error');
+    const errorMsg = $(error).find('.error-msg');
+    const button = $(tr).find('.suspend');
+
+    $.ajax({
+      url: action,
+      type: 'POST',
+      beforeSend: function() {
+        $(button).attr('disabled', '');
+      }
+    }).done(function(response) {
+      if (response.length !== 0) {
+        $(error).removeClass('d-none');
+        $(error).addClass('d-block');
+        $(errorMsg).html('Ops! Algo de errado aconteceu!').fadeIn();
+      } else {
+        window.location.href = `${DIRPAGE}userspanel`;
+      }
+    }).fail(function() {
+      $(error).removeClass('d-none');
+      $(error).addClass('d-block');
+      $(errorMsg).html('Ops! Algo de errado aconteceu!').fadeIn();
+    }).always(function() {
+      $(button).removeAttr('disabled');
+    });
+  });
+});
