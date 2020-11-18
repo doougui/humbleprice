c038798380fcbf0d8546e43d60b0440f73c30db8
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2020-11-18 12:30:51 -0300

    Using dirname funcion instead of setting a global DIRREQ const

--- config/config.php
+++ config/config.php
@@ -2,23 +2,16 @@
 
 require('environment.php');
 
-$innerFolder = "humbleprice_tcc/";
-define('DIRPAGE',"http://{$_SERVER['HTTP_HOST']}/{$innerFolder}");
-
-if (substr($_SERVER['DOCUMENT_ROOT'], -1) == '/') {
-    define('DIRREQ', "{$_SERVER['DOCUMENT_ROOT']}{$innerFolder}");
-} else {
-    define('DIRREQ', "{$_SERVER['DOCUMENT_ROOT']}/{$innerFolder}");
-}
+define('DIRPAGE',"http://{$_SERVER['HTTP_HOST']}/");
 
 // Global folders
-define('DIRIMG', DIRPAGE."public/img/");
-define('DIRCSS', DIRPAGE."public/css/");
-define('DIRJS', DIRPAGE."public/js/");
-define('DIRVID', DIRPAGE."public/video/");
-define('DIRAUD', DIRPAGE."public/audio/");
-define('DIRFONT', DIRPAGE."public/font/");
-define('DIRDESIGN', DIRPAGE."public/design/");
+define('DIRIMG', DIRPAGE."img/");
+define('DIRCSS', DIRPAGE."css/");
+define('DIRJS', DIRPAGE."js/");
+define('DIRVID', DIRPAGE."video/");
+define('DIRAUD', DIRPAGE."audio/");
+define('DIRFONT', DIRPAGE."font/");
+define('DIRDESIGN', DIRPAGE."design/");
 
 // Database connection
 $config = [];
